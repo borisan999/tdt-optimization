68de270bc775f5282dbe320bae5e5965e6116549
boris <boris@carbon.com.co>
2026-02-24 15:40:24 -0500

    style: add animate.css for UI animations

--- public/templates/header.php
+++ public/templates/header.php
@@ -14,6 +14,7 @@ if (headers_sent()) {
     <link rel="stylesheet" href="assets/css/app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
