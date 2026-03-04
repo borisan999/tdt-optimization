a7fd057ecd05ea92b624d9f8474752e83bcb35ac
boris <boris@carbon.com.co>
2026-03-04 12:07:15 -0500

    Security: Remove sensitive documentation and Python logic from tracking; add tech docs script

--- app/helpers/Translation.php
+++ app/helpers/Translation.php
@@ -56,8 +56,14 @@ namespace app\helpers {
 }
 
 namespace {
-    // Global helper function for brevity
     if (!function_exists('__')) {
+        /**
+         * Global helper for translation.
+         *
+         * @param string $key
+         * @param array $placeholders
+         * @return string
+         */
         function __(string $key, array $placeholders = []): string
         {
             return \app\helpers\Translation::get($key, $placeholders);
